e5b70fa595e8f3f956122528606e51b080f1bf19
zyke <zyke.victoria.cics@ust.edu.ph>
2023-11-17 13:29:27 +0800

    11/17 1:29PM

--- app/src/main/java/com/example/grpproj/MainActivity.kt
+++ app/src/main/java/com/example/grpproj/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.grpproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var loginBtn : Button = findViewById(R.id.btnLogin)
+
+        loginBtn.setOnClickListener {
+            loginBtn.isSelected != loginBtn.isSelected
+        }
+
     }
 }
